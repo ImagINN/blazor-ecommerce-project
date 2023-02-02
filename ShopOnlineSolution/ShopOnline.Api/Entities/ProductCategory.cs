444840215a40a11e4c5917d05d62236152d3b631
ImagINN <muhammed.gokhan55@gmail.com>
2023-02-02 13:57:57 +0300

    V1

---
+++ ShopOnlineSolution/ShopOnline.Api/Entities/ProductCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopOnline.Api.Entities
+{
+    public class ProductCategory
+    {
+        // Public Features Area
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IconCSS { get; set; }        
+    }
+}
---
+++ ShopOnlineSolution/ShopOnline.Models/Dtos/CartItemToAddDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnline.Models.Dtos
+{
+    public class CartItemToAddDto
+    {
+        public int CartId { get; set; }    /*İlk versiyon : Id , Benim yaptığım CartId*/
+        public int ProductId { get; set; }
+        public int Qty { get; set; }
+    }
+}
